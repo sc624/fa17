fe804fc723731c52cf5fc1ce5112e07fcb328245
samuelc3 <samuelc3@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-09-04 21:57:30 +0000

    hey

--- samuelc3/mp1/mp1.cpp
+++ samuelc3/mp1/mp1.cpp
@@ -1,5 +1,18 @@
 #include <string>
-
+#include "cs225/PNG.h"
+#include "mp1.h"
+using namespace cs225;
 void rotate(std::string inputFile, std::string outputFile) {
-
+	PNG input;
+	PNG output;
+	input.readFromFile(inputFile);
+	int imagewidth = input.width();
+	int imageheight = input.height();
+	for(unsigned x = 0; x < imagewidth; x++){
+		for(unsigned y = 0; x < imageheight; y++){
+		HSLAPixel *inputPixel = input.getPixel(x,y);
+		HSLAPixel *outputPixel = output.getPixel(imagewidth - x, imageheight - y);
+		}
+	}
+	 output.writeToFile(outputFile);
 }
