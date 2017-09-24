fe2f670c140b416eb8683f96db6d2a07e9f686f2
samuelc3 <samuelc3@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-09-24 02:49:53 +0000

    mp2

---
+++ samuelc3/mp2/StickerSheet.h
@@ -0,0 +1,25 @@
+#ifndef STICKERSHEET_H
+#define STICKERSHEET_H
+using namespace std;    
+class StickerSheet{
+        public:
+            StickerSheet (const Image &picture, unsigned max);
+            ~StickerSheet ();
+            StickerSheet (const StickerSheet &other);
+            const StickerSheet &operator= (const StickerSheet &other);
+            void changeMaxStickers (unsigned max);
+            int addSticker (Image &sticker, unsigned x, unsigned y);
+            bool translate (unsigned index, unsigned x, unsigned y);
+            void removeSticker (unsigned index);
+            Image *getSticker (unsigned index) const;
+            Image render () const;
+            void copyHelper(const StickerSheet &other);
+            void destroy_();
+        private:
+            unsigned maxpapushin;
+            Image ** binky[];
+            unsigned *coordinate[][];
+            unsigned *lay[];
+};
+
+#endif
