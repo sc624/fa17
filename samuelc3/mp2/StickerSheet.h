181b47b00788c7f23d631f18e2a08b30817ff823
samuelc3 <samuelc3@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-09-25 01:12:43 +0000

    awefiajwef

--- samuelc3/mp2/StickerSheet.h
+++ samuelc3/mp2/StickerSheet.h
@@ -29,3 +29,32 @@ class StickerSheet{
 };
 
 #endif
+
+
+/*
+ *Image StickerSheet::render() const {
+   Image * curObj;
+     Image * retImg = new Image(base);
+       for(unsigned x = 0; x < arr_cnt; x++) {
+           curObj = stickArr[x];
+
+for(unsigned xC = xVal[x]; xC - xVal[x] < curObj->width(); xC++) {
+    for(unsigned yC = yVal[x]; yC - yVal[x] < curObj->height(); yC++) {
+    if(yC >= retImg->height()) {
+        retImg->scale((retImg->height() + 1)/(retImg->height()));
+        }
+            if((curObj->getPixel(xC - xVal[x], yC - yVal[x]))->a != 0) {
+(retImg->getPixel(xC,yC))->h = (curObj->getPixel(xC - xVal[x], yC - yVal[x]))->h;
+(retImg->getPixel(xC,yC))->s = (curObj->getPixel(xC - xVal[x], yC - yVal[x]))->s;
+(retImg->getPixel(xC,yC))->l = (curObj->getPixel(xC - xVal[x], yC - yVal[x]))->l;
+(retImg->getPixel(xC,yC))->a = (curObj->getPixel(xC - xVal[x], yC - yVal[x]))->a;
+                                                                                                       }
+                                                                                                             }
+                                                                                                                   if(xC >= retImg->width()) {
+                                                                                                                           retImg->scale((retImg->width() + 1)/(retImg->width()));
+                                                                                                                                 }
+                                                                                                                                     }
+                                                                                                                                       }
+                                                                                                                                         return *retImg;
+                                                                                                                                         }
+*/
