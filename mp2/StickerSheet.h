8d6c021c33cd5fe094c04109f44948f012059e41
samuelc3 <samuelc3@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-09-25 03:37:02 +0000

    hopefully done

--- mp2/StickerSheet.h
+++ mp2/StickerSheet.h
@@ -22,7 +22,8 @@ class StickerSheet{
 
         private:
             unsigned maxpapushin;
-            Image ** binky;
+            Image * pants;
+            Image * binky;
             unsigned *Xcoordinate;
             unsigned *Ycoordinate;
             unsigned current;
@@ -32,29 +33,34 @@ class StickerSheet{
 
 
 /*
- *Image StickerSheet::render() const {
-   Image * curObj;
-     Image * retImg = new Image(base);
-       for(unsigned x = 0; x < arr_cnt; x++) {
-           curObj = stickArr[x];
+#ifndef STICKER_SHEET_H_
+#define STICKER_SHEET_H_
 
-for(unsigned xC = xVal[x]; xC - xVal[x] < curObj->width(); xC++) {
-    for(unsigned yC = yVal[x]; yC - yVal[x] < curObj->height(); yC++) {
-    if(yC >= retImg->height()) {
-        retImg->scale((retImg->height() + 1)/(retImg->height()));
-        }
-            if((curObj->getPixel(xC - xVal[x], yC - yVal[x]))->a != 0) {
-(retImg->getPixel(xC,yC))->h = (curObj->getPixel(xC - xVal[x], yC - yVal[x]))->h;
-(retImg->getPixel(xC,yC))->s = (curObj->getPixel(xC - xVal[x], yC - yVal[x]))->s;
-(retImg->getPixel(xC,yC))->l = (curObj->getPixel(xC - xVal[x], yC - yVal[x]))->l;
-(retImg->getPixel(xC,yC))->a = (curObj->getPixel(xC - xVal[x], yC - yVal[x]))->a;
-}
-}
-if(xC >= retImg->width()) {
-                                                                                                                           retImg->scale((retImg->width() + 1)/(retImg->width()));
-                                                                                                                                 }
-                                                                                                                                     }
-                                                                                                                                       }
-                                                                                                                                         return *retImg;
-                                                                                                                                         }
+#include "Image.h"
+
+using namespace cs225;
+
+class StickerSheet {
+            public:
+                            StickerSheet(const Image &picture, unsigned max);
+                                    ~StickerSheet();
+                                            StickerSheet(const StickerSheet &other);
+                                                    const StickerSheet &operator=(const StickerSheet &other);
+                                                            void changeMaxStickers(unsigned max);
+                                                                    int addSticker(Image &sticker, unsigned x, unsigned y);
+                                                                            bool translate(unsigned index, unsigned x, unsigned y);
+                                                                                    void removeSticker(unsigned index);
+                                                                                            Image* getSticker(unsigned index) const;
+                                                                                                    Image render() const;
+                                                                                                        private:
+                                                                                                            Image* imageList_;      
+                                                                                                                    int* xList_;
+                                                                                                                            int* yList_;
+                                                                                                                                    unsigned max_;
+                                                                                                                                            unsigned size_;
+
+                                                                                                                                                    void _copy(StickerSheet const &other);
+};
+
+#endif
 */
