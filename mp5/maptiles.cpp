918cefa25dc2995ecd1e21b63d35d7fe3a56efd9
samuelc3 <samuelc3@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-11-08 02:00:52 +0000

    awefawef

--- mp5/maptiles.cpp
+++ mp5/maptiles.cpp
@@ -7,55 +7,43 @@ using namespace std;
 
 MosaicCanvas * mapTiles(SourceImage const & theSource, vector<TileImage> const & theTiles)
 {
-    vector< Point<3> > colors;
-     vector< TileImage >::iterator it;
+  int rows = theSource.getRows();
+  int columns = theSource.getColumns();
+//cout<<"reach 19"<<endl;
+MosaicCanvas * finalMosaic = new MosaicCanvas(rows, columns);
+vector <Point<3> > pixelVector;
+map <Point<3>, int> indexMap;
+//cout<<"reach 22"<<endl;
+for(size_t i = 0; i < theTiles.size(); i++)
+{
+  HSLAPixel pi = theTiles[i].getAverageColor();
+          Point<3> po(pi.h, pi.s, pi.l);
 
-     //Build a map between average color and the tile that would generate the average color
-     map<Point<3>, TileImage> theMap;
+          //map it
+          indexMap[po] = theTiles[i];
 
-     //Loop through the tiles
-     //for (it = theTiles.begin(); it != theTiles.end(); it++)
-     for (size_t i = 0; i < theTiles.size(); i++)
-     {
-         //get average color
-         HSLAPixel pi = theTiles.at(i).getAverageColor();
-         Point<3> po(pi.h, pi.s, pi.l, pi.a);
+          //put in vector
+          pixelVector.push_back(po);
+}
 
-         //map it
-         theMap[po] = theTiles.at(i);
+for (int i = 0; i < r; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+        //for each tile:
+        //grab the pixel from source
+        RGBAPixel regionColor = theSource.getRegionColor(i, j);
+        //find the closest average color from kdtree using NNS
+        Point<3> regionPoint(regionColor.red, regionColor.green, regionColor.blue);
+        Point<3> closestPoint = tree.findNearestNeighbor(regionPoint);
+        //use the map to find the tile
+        TileImage theTile = theMap[closestPoint];
 
-         //put in vector
-         colors.push_back(po);
-     }
+        //put the tile onto canvas
+        canvas->setTile(i,j,theTile);
+    }
+}
 
-     //Call kdtree constructor
-     KDTree<3>::KDTree tree(colors);
-
-     //Start building the canvas
-     //Declare a new canvas
-     int r = theSource.getRows();
-     int c = theSource.getColumns();
-     MosaicCanvas::MosaicCanvas *canvas = new MosaicCanvas(r, c);
-
-     //loop through each tile
-     for (int i = 0; i < r; i++)
-     {
-         for (int j = 0; j < c; j++)
-         {
-             //for each tile:
-             //grab the pixel from source
-             HSLAPixel regionColor = theSource.getRegionColor(i, j);
-             //find the closest average color from kdtree using NNS
-             Point<3> regionPoint(regionColor.h, regionColor.s, regionColor.l);
-             Point<3> closestPoint = tree.findNearestNeighbor(regionPoint);
-             //use the map to find the tile
-             TileImage theTile = theMap[closestPoint];
-
-             //put the tile onto canvas
-             canvas->setTile(i,j,theTile);
-         }
-     }
-
-     //return canvas
-     return canvas;
+//return canvas
+return canvas;
 }
